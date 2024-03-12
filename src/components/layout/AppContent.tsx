de2d1e7deb903524d983e203a6dbedf309440235
Egor Ushakov <witluv@icloud.com>
2024-03-12 21:53:03 +0300

    refactor: move the component to a separate module

---
+++ src/components/layout/AppContent.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Layout } from 'antd';
+
+const contentStyle: React.CSSProperties = {
+  textAlign: 'center',
+  minHeight: 'calc(100vh - 60px)',
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#001529',
+  padding: '1rem',
+};
+
+export default function AppContent() {
+  return <Layout.Content style={contentStyle}>Content</Layout.Content>;
+}
