d0cfb9575d87673faa2ded026497becff403e614
Egor Ushakov <witluv@icloud.com>
2024-03-13 22:20:50 +0300

    refactor: move the layout to a separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
-import { Layout } from 'antd';
-import AppSider from './components/layout/AppSider';
-import AppHeader from './components/layout/AppHeader';
-import AppContent from './components/layout/AppContent';
+import { CryptoContextProvider } from './context/crypto-context';
+import AppLayout from './components/layout/AppLayout';
 
 export default function App() {
   return (
-    <Layout>
-      <AppHeader />
-      <Layout>
-        <AppSider />
-        <AppContent />
-      </Layout>
-    </Layout>
+    <CryptoContextProvider>
+      <AppLayout />
+    </CryptoContextProvider>
   );
 }
